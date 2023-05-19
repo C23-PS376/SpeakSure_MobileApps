a6786347c1024d2703072eb2099c684037f89e53
AsimaPrimaYohanaT <asimap.y.tampubolon@gmail.com>
2023-05-19 22:22:36 +0700

    tampilan register

---
+++ app/src/main/java/com/example/speaksure_capstone/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.speaksure_capstone.register
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.speaksure_capstone.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
