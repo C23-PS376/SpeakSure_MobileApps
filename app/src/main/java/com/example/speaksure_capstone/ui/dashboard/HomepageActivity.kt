f80941a24d11e4a73ed412a883ac9e618c65110f
Adriel O Pandiangan <adrielopandiangan@gmail.com>
2023-05-30 15:48:25 +0700

    update theme

--- app/src/main/java/com/example/speaksure_capstone/ui/dashboard/HomepageActivity.kt
+++ app/src/main/java/com/example/speaksure_capstone/ui/dashboard/HomepageActivity.kt
@@ -15,46 +15,31 @@ class HomepageActivity : AppCompatActivity() {
         binding = ActivityHomepageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /*val fragmentManager =supportFragmentManager
-// Buat instance HomeFragment
-        val homeFragment = HomeFragment()
 
-// Cek apakah fragment sudah ada sebelumnya
-        val fragment = fragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
-        if (fragment !is HomeFragment) {
-            // Jika fragment belum ada, tambahkan menggunakan childFragmentManager dari container yang sesuai dalam binding
-            fragmentManager.beginTransaction()
-                .add(binding.frameContainer.id, homeFragment, HomeFragment::class.java.simpleName)
-                .commit()
-        }*/
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as? NavHostFragment
+        val navController = navHostFragment?.navController
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        if (navController != null) {
+            val bottomNavigationView = binding.bottomNavigation
+            bottomNavigationView.setupWithNavController(navController)
 
-
-        val bottomNavigationView = binding.bottomNavigation
-
-        bottomNavigationView.setupWithNavController(navController)
-
-        binding.bottomNavigation.setOnItemSelectedListener{ item ->
-            when(item.itemId) {
-                R.id.home_nav -> {
-                    navController.navigate(R.id.navigation_home)
-
-                    true
+            binding.bottomNavigation.setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.home_nav -> {
+                        navController.navigate(R.id.navigation_home)
+                        true
+                    }
+                    R.id.add_nav -> {
+                        navController.navigate(R.id.navigation_add_thread)
+                        true
+                    }
+                    R.id.profile_nav -> {
+                        navController.navigate(R.id.navigation_profile)
+                        true
+                    }
+                    else -> false
                 }
-                R.id.add_nav -> {
-                    navController.navigate(R.id.navigation_add_thread)
-
-                    true
-                }
-                R.id.profile_nav -> {
-                    navController.navigate(R.id.navigation_profile)
-                    true
-                }
-                else -> false
             }
         }
     }
-
 }
